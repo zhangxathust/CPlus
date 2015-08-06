caad072a036e4c6534654112861ec341664c97cb
zhangxathust <zhanxnj@163.com>
2015-08-07 00:26:37 +0800

    add friend inline override operator method

--- mytime0.cpp
+++ mytime0.cpp
@@ -41,7 +41,47 @@ Time Time::sum(const Time & t) const
 	return sum;
 }
 
+Time Time::operator+(const Time & t) const
+{
+	Time sum;
+	sum.minutes = minutes + t.minutes;
+	sum.hours = hours + t.hours + sum.minutes / 60;
+	sum.minutes %= 60;
+	return sum;
+}
+
+Time Time::operator-(const Time & t) const
+{
+	Time minus;
+	if (minutes < t.minutes)
+	{
+		minus.hours = hours - t.hours -1;
+		minus.minutes = 60 + minutes - t.minutes;
+	}
+	else
+	{
+		minus.hours = hours - t.hours;
+		minus.minutes = minutes - t.minutes;
+	}
+	return minus;
+}
+
+Time Time::operator*(double n) const
+{
+	Time multiply;
+	multiply.minutes = n * minutes;
+	multiply.hours = n * hours + multiply.minutes / 60;
+	multiply.minutes %= 60;
+	return multiply;
+}
+
 void Time::show() const
 {
 	cout << hours << " hours, " << minutes << " minutes!";
+}
+
+std::ostream & operator<<(std::ostream & os, const Time & t)
+{
+	os << "hours: " << t.hours << " minutes: " << t.minutes;
+	return os;
 }
--- mytime0.h
+++ mytime0.h
@@ -16,7 +16,20 @@ public:
 	void addHour(int h);
 	void reset(int h = 0, int m = 0);
 	Time sum(const Time & t) const;
+	// override operator + - *
+	Time operator+(const Time & t) const;
+	Time operator-(const Time & t) const;
+	Time operator*(double n) const;	//this only useful to time * n
+	// friend method. used to n * time. it is also an inline method.
+	friend Time operator*(double n, const Time & t)
+	{
+		// call method operator*(double n).
+		return t * n;
+	}
+
 	void show() const;
+	//an method override "<<" ,it is like java.lang.Object.toString().
+	friend std::ostream & operator<<(std::ostream & os, const Time & t);
 };
 
 #endif
